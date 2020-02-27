078f59a5333709222046db39480b37aa68439d11
LariXii <gatien.continsouzas@edu.univ-fcomte.fr>
2020-02-27 11:36:33 +0100

    Gatien
    Premier commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from dblp_parser_graph import *
+import matplotlib.pyplot as plt
+from networkx import nx
+
+dblp_path = 'output.xml'
+
+G = nx.DiGraph()
+n, e = parse_article_to_graph(dblp_path)
+
+G.add_nodes_from(n)
+G.add_edges_from(e)
+
+for u, v in G.edges():
+    print("Source : %s / Destination : %s" % (u, v))
+
+#print(list(G.nodes.data()))
+
+color_map = []
+
+for n in G.nodes():
+    if G.nodes[n]['parti'] == 'author':
+        color_map.append('red')
+    else:
+        color_map.append('blue')
+
+options = {
+    'node_color': color_map,
+    'node_size': 50,
+    'line_color': 'grey',
+    'linewidths': 0,
+    'width': 0.1,
+}
+
+pos = nx.circular_layout(G)
+nx.draw(G, pos, font_size=16, with_labels=False, **options)
+for p in pos:  # raise text positions
+    pos[p][1] += 0.07
+nx.draw_networkx_labels(G, pos)
+plt.show()
