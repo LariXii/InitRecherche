1e1f51013c016cbf97447061270f0bb8614b1b46
LariXii <gatien.continsouzas@edu.univ-fcomte.fr>
2020-03-23 15:16:04 +0100

    Gatien

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 from dblp_parser_graph import *
 import matplotlib.pyplot as plt
 from networkx import nx
-import networkx
 
 dblp_path = 'resources/output.xml'
 
 journals, impacts = parse_journal('resources/journals.xml')
 
-#print(journals)
-
 G = nx.DiGraph()
-#n, e = parse_article_to_graph(dblp_path)
+
 n, e = parse_art_aut_by_journals('resources/articles.xml', journals)
 
 G.add_nodes_from(n)
 G.add_edges_from(e)
 
-#for u, v in G.edges():
-#    print("Source : %s / Destination : %s" % (u, v))
-
-#print(len(G.nodes()))
-
-#for u, v, action in G.edges(data='action'):
-#    if action is not None:
-#        print("(%s) [%s] (%s)" % (u, action, v))
 
 color_map = []
-
 for n in G.nodes():
     if G.nodes[n]['parti'] == 'author':
         color_map.append('red')
@@ -38,7 +26,7 @@ for n in G.nodes():
 
 for c, v in impacts.items():
     try:
-        G.nodes[c]['impact_factor'] = v
+        G.nodes[c]['impact_factor'] = float(v)
     except KeyError:
         continue
 
@@ -55,12 +43,10 @@ for noeud in x :
         poids_articles.append(noeud)
     else :
         continue
-#print(poids_articles)
+
 liste_auteurs = []
-
-
 for n in G.nodes() :
-    if G.nodes()[n]['parti'] == 'author' :
+    if G.nodes[n]['parti'] == 'author' :
         liste_auteurs.append(n)
     else :
         continue
@@ -68,7 +54,7 @@ for n in G.nodes() :
 # --> Articles écrits par auteur.
 auteur_article = dict()
 for noeud in G :
-    if G.nodes()[noeud]['parti'] == 'author' :
+    if G.nodes[noeud]['parti'] == 'author':
         #print("\nVoisin de {} est {}".format(noeud, list(G.neighbors(noeud))))
         auteur_article[noeud] = list(G.neighbors(noeud))
     else :
@@ -77,7 +63,7 @@ for noeud in G :
 # --> Articles écrits pour quel journal
 journal_article = dict()
 for noeud in G :
-    if G.nodes()[noeud]['parti'] == 'journal' :
+    if G.nodes[noeud]['parti'] == 'journal':
         #print("\nVoisin de {} est {}".format(noeud, list(G.neighbors(noeud))))
         journal_article[noeud] = list(G.neighbors(noeud))
     else :
@@ -87,21 +73,23 @@ for noeud in G :
 
 journal_impact = dict()
 for noeud in G :
-    if G.nodes()[noeud]['parti'] == 'journal' :
-        journal_impact[noeud] = G.nodes()[noeud]['impact_factor']
+    if G.nodes[noeud]['parti'] == 'journal':
+        journal_impact[noeud] = G.nodes[noeud]['impact_factor']
     else :
         continue
 
 # --> Attribuer à chaque article, un poids égale à l'impact factor de son journal.
 liste_journaux = journal_impact.keys()
-t = []
 impact_article = dict()
-for journal in liste_journaux :
-    if journal in journal_article.keys() :
+for journal in liste_journaux:
+    if journal in journal_article.keys():
         impact_article[journal_impact[journal]] = journal_article[journal]
-    else :
+    else:
         continue
 
+for k, v in impact_article.items():
+    for article in v:
+        G.nodes[article]['impact_factor'] = float(k)
 
 
 # --> Attribuer à chaque auteur un coeff. || ('Auteur' : Coefficient)
@@ -109,15 +97,10 @@ auteur_coef = dict()
 nombre_articles = 0
 somme_impact_articles = 0
 
-#for impact in impact_article.values() :
-#    for article in impact :
-#        print("Article : {} est présent dans {}".format(article, impact))
-
 
 for auteur in auteur_article.keys() : #L'auteur
     nombre_articles = len(auteur_article[auteur]) #Nombre d'articles ayant été écrits par l'auteur.
     for articles in auteur_article[auteur] : #Article par article
-
         for articles_impact_article in impact_article.values() : #Check si l'article est présent dans la liste où on a fait la correspondance impact - article
             if articles in articles_impact_article : #Si l'article a un impact, on le rajoute à la somme pour calculer le coeff.
                 for cle in impact_article.keys() : #On parcourt la liste selon l'impact des articles.
@@ -128,7 +111,7 @@ for auteur in auteur_article.keys() : #L'auteur
                         somme_impact_articles = 0
                     else :
                         continue
-            else :
+            else:
                 continue
 
 list_nodes = dict()
@@ -138,26 +121,22 @@ for author in auteur_coef.keys() :
             list_nodes[node] = auteur_coef[author]*5
             continue
 
-difference = set(G.nodes().keys()) - set(list_nodes.keys())
-for d in difference :
-    list_nodes[d] = 1
+for author in auteur_coef.keys():
+    G.nodes[author]['impact_factor'] = auteur_coef[author]*5
 
 options = {
     'node_color': color_map,
-    #'node_size': 50,
     'line_color': 'grey',
     'linewidths': 0,
     'width': 0.1,
 }
 
+#Liste des poids de tous les noeuds
+list_weight = []
+for n, c in G.nodes.data('impact_factor'):
+    list_weight.append(c)
 
-liste_des_noeuds = list(list_nodes.keys())
-liste_des_poids = list(list_nodes.values())
 
 pos = nx.spring_layout(G)
-#nx.draw(G, pos, font_size=16, with_labels=False, **options)
-nx.draw(G, pos, font_size=16, nodelist=liste_des_noeuds, node_size=[v * 100 for v in liste_des_poids], with_labels=True, **options)
-#for p in pos:  # raise text positions
-#    pos[p][1] += 0.07
-#nx.draw_networkx_labels(G, pos)
+nx.draw(G, pos, font_size=6, node_size=[c*100 for c in list_weight], with_labels=True, **options)
 plt.show()
